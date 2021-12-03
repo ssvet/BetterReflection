d759a721df7c59b819f90201cfbfd5c6c675597d
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-03 23:39:12 +0100

    Skip Enum adapter tests before PHP 8

--- test/unit/Reflection/Adapter/ReflectionEnumTest.php
+++ test/unit/Reflection/Adapter/ReflectionEnumTest.php
@@ -37,11 +37,23 @@ use function array_map;
 use function get_class_methods;
 use function is_array;
 
+use const PHP_VERSION_ID;
+
 /**
  * @covers \Roave\BetterReflection\Reflection\Adapter\ReflectionEnum
  */
 class ReflectionEnumTest extends TestCase
 {
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        if (PHP_VERSION_ID >= 80000) {
+            return;
+        }
+
+        self::markTestSkipped('Test requires PHP 8.0');
+    }
+
     public function coreReflectionMethodNamesProvider(): array
     {
         $methods = get_class_methods(CoreReflectionEnum::class);
